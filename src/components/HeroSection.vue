cbb96bfbb139165fdbed8deb9b66b8012debcb39
Norman <peters.norman91@gmail.de>
2024-08-16 15:08:09 +0200

    feat: Adjust layout to display image above text on smaller screens

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,8 +1,13 @@
 <template>
   <section class="relative flex flex-col md:flex-row items-center justify-between mobile:mt-4 sm:mt-16">
+    <!-- Image Section -->
+    <div class="relative overflow-hidden w-full mt-12 md:mt-0 md:w-auto md:absolute md:right-0 md:top-0 order-1 md:order-2">
+      <img :src="illustrationWorking" alt="Working Illustration" class="w-full max-w-none md:max-w-xl object-cover md:object-contain translate-x-10 sm:translate-x-16 md:translate-x-24">
+    </div>
+
     <!-- Text Section -->
-    <div class="text-center md:text-left md:max-w-xl">
-      <h1 class="text-6xl md:text-7xl font-bold text-veryDarkViolet leading-tight">
+    <div class="text-center md:text-left md:max-w-xl order-2 md:order-1">
+      <h1 class="text-4xl md:text-7xl font-bold text-veryDarkViolet leading-tight">
         More than just<br> shorter links
       </h1>
       <p class="text-grayishViolet text-lg my-2 md:my-4">
@@ -12,13 +17,9 @@
         Get Started
       </button>
     </div>
-
-    <!-- Image Section -->
-    <div class="relative overflow-hidden w-full mt-12 md:mt-0 md:w-auto md:absolute md:right-0 md:top-0">
-      <img :src="illustrationWorking" alt="Working Illustration" class="w-full max-w-none md:max-w-xl object-cover md:object-contain translate-x-10 sm:translate-x-16 md:translate-x-24">
-    </div>
   </section>
 </template>
+
 
 <script setup>
 import illustrationWorking from "@/assets/images/illustration-working.svg";
