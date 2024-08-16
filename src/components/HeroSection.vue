c20d3604539cb2ecc1f4e88cacaa4196d95df3ca
Norman <peters.norman91@gmail.de>
2024-08-16 10:01:51 +0200

    Setup Tailwind CSS and integrated front-end styles

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,8 +3,9 @@
 </script>
 
 <template>
-  <div></div>
-
+  <div class="text-veryDarkBlue font-poppins">
+    <h1 class="text-4xl font-bold">Primary Heading</h1>
+  </div>
 </template>
 
 <style scoped>
